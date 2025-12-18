97de5fd7b8de26ccd404c0045a5de0ae10681f1a
AJ V <avaithyam@yahoo.com>
2025-12-17 23:01:32 -0500

    think required is mostly done

--- src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -1,11 +1,19 @@
 //package org.yearup.data.mysql;
 //
 //import org.yearup.data.ShoppingCartDao;
+//import org.yearup.models.ShoppingCart;
 //
 //import javax.sql.DataSource;
 //
 //public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDao {
 //    public MySqlShoppingCartDao(DataSource dataSource) {
 //        super(dataSource);
+//    }
+//
+//    @Override
+//    public ShoppingCart getByUserId(int userId) {
+//        ShoppingCart cart = new ShoppingCart();
+//
+//        String sql =
 //    }
 //}
